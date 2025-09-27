e0b706c190cdc6313904ae9f094ccaaad25e8dc7
SolWeintraub95 <shutov9597@gmail.com>
2025-09-27 16:44:36 +0300

    Tested and bugfixed v5 Now its really the very bestest version

--- v5.py
+++ v5.py
@@ -1,19 +1,15 @@
-class EncryptorBase:
-    def __init__(self, shift: int):
-        self.shift: int = shift
-
-
-class Encryptor(EncryptorBase):
-    def __init__(self):
+class Encryptor:
+    def __init__(self, shift):
         self.alphabet: str = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
         self.alphabet_dict: dict = {letter: index for index, letter in enumerate(self.alphabet)}
+        self.shift = shift
 
     def shift_text(self, text: str) -> str:
         result: list = []
         letters_quantity: int = len(self.alphabet)
         for char in text:
             low_char = char.lower()
-            if self.alphabet_dict.get(low_char):
+            if low_char in self.alphabet_dict:  # ✅ проверка по ключу
                 new_index: int = (self.alphabet_dict[low_char] + self.shift) % letters_quantity
                 new_char = self.alphabet[new_index].upper() if char.isupper() else self.alphabet[new_index]
                 result.append(new_char)
@@ -24,5 +20,6 @@ class Encryptor(EncryptorBase):
 
 if __name__ == '__main__':
     change = int(input('Введите число на которое будут смещаться буквы: '))
-    shifter = Encryptor(change)  # сдвиг на указанное количество букв
-    print(shifter.shift_text('К сожалению, Вы не прошли техническое интервью.\nУвидимся через полгода.'))
+    text = str(input('Введите текст: '))
+    shifter = Encryptor(change)
+    print(shifter.shift_text(text))
