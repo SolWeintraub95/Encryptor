00f46841277ec3bdfcffbb120ac43b0902cd224a
SolWeintraub95 <shutov9597@gmail.com>
2025-09-27 17:12:19 +0300

    Added choice between inputting text and text file

--- v5.py
+++ v5.py
@@ -4,10 +4,10 @@ class Encryptor:
         self.alphabet_dict: dict = {letter: index for index, letter in enumerate(self.alphabet)}
         self.shift = shift
 
-    def shift_text(self, text: str) -> str:
+    def shift_text(self, input_text: str) -> str:
         result: list = []
         letters_quantity: int = len(self.alphabet)
-        for char in text:
+        for char in input_text:
             low_char = char.lower()
             if low_char in self.alphabet_dict:  # ✅ проверка по ключу
                 new_index: int = (self.alphabet_dict[low_char] + self.shift) % letters_quantity
@@ -16,10 +16,18 @@ class Encryptor:
             else:
                 result.append(char)
         return ''.join(result)
+input_text = """
 
+"""
 
 if __name__ == '__main__':
-    change = int(input('Введите число на которое будут смещаться буквы: '))
-    text = str(input('Введите текст: '))
-    shifter = Encryptor(change)
-    print(shifter.shift_text(text))
+    shift = int(input('Введите число на которое будут смещаться буквы: '))
+    input_type = int(input('Ввести текст или взять из текстового документа (1,2): '))
+    shifter = Encryptor(shift)
+    if input_type == 1:
+        input_text = str(input('Введите текст: '))
+        print(shifter.shift_text(input_text))
+    else:
+        file=open('user_input.txt', 'r', encoding='utf-8')
+        input_text = file.read()
+        print(shifter.shift_text(input_text))
